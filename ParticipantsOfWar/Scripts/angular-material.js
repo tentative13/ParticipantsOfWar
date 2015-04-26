5d11f3a94064dc816c189524da7468a4c6fa479f
tentative13 <alex.regn@gmail.com>
2015-04-26 20:12:01 +0500

    enabled brown team

--- ParticipantsOfWar/Scripts/angular-material.js
+++ ParticipantsOfWar/Scripts/angular-material.js
@@ -28,8 +28,13 @@ function MdCoreConfigure($provide, $mdThemingProvider) {
   $provide.decorator('$$rAF', ["$delegate", rAFDecorator]);
 
   $mdThemingProvider.theme('default')
-    .primaryPalette('indigo')
-    .accentPalette('pink')
+    .primaryPalette('brown', {
+        'default': '400', // by default use shade 400 from the pink palette for primary intentions
+        'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+        'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+        'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+    })
+    .accentPalette('light-blue')
     .warnPalette('red')
     .backgroundPalette('grey');
 }
